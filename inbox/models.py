7907b9f5d4b2185482df9ba8f7e90f9d19ce2b35
Samandar <samandarnayimjonov@gmail.com>
2025-02-16 17:08:59 +0200

    add slug url

--- inbox/models.py
+++ inbox/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.shortcuts import reverse
+from django.utils.text import slugify
 
 
 class Message(models.Model):
@@ -10,6 +11,17 @@ class Message(models.Model):
     message = models.TextField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.from_email)
+        super(Message, self).save(*args, **kwargs)
 
     def get_detail_url(self):
-        return reverse('inbox:detail', args=[self.pk])
+        return reverse('inbox:detail', args=[
+            self.created_at.year,
+            self.created_at.month,
+            self.created_at.day,
+            self.slug
+        ])
