f07c2ff4cfda1046740c7e4a572f6ca4f572a8ae
Samandar <samandarnayimjonov@gmail.com>
2025-02-16 16:48:38 +0200

    added admin panel

--- inbox/admin.py
+++ inbox/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('from_email', 'to_email', 'subject', 'user', 'created_at')
+    list_filter = ('created_at', 'user')
+    search_fields = ('from_email', 'to_email', 'subject', 'message')
+    ordering = ('-created_at',)
+
+
+admin.site.register(Message, MessageAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'full_name', 'is_active', 'is_staff', 'date_joined')
+    list_filter = ('is_active', 'is_staff', 'date_joined')
+    search_fields = ('email', 'full_name')
+    ordering = ('-date_joined',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('full_name',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('date_joined',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'full_name', 'password1', 'password2', 'is_active', 'is_staff')}
+        ),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
